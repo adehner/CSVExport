180f7f20ccebdfe8998f4a934888a3064c5dfa2b
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-01-28 00:00:00 +0100

    Set media type and extension for tsv.

--- views/admin/export/csv.php
+++ views/admin/export/csv.php
@@ -6,30 +6,34 @@
  * @var string $title
  */
 
+$delimiter = get_option('csv_export_delimiter') ?: ',';
+if ($delimiter === 'tab') {
+    $delimiter = "\t";
+    $extension = 'tsv';
+    $mediaType = 'text/tab-separated-values';
+} else {
+    $extension = 'csv';
+    $mediaType = 'text/csv';
+}
+$enclosure = get_option('csv_export_enclosure') ?: '"';
+
 $title = option('site_title');
 $title = preg_replace('/[^A-Za-z0-9-]/', '_', $title);
 $title = preg_replace('/_+/', '_', $title);
 $title = substr($title, 0, 16);
 $title = urlencode($title);
+
 // date_default_timezone_set('America/Los_Angeles');
-if ($search) {
-    $fileName = $title . '-Export-' . date('Ymd-His') . '.csv';
-} else {
-    $fileName = $title . '-Full_Export-' . date('Ymd-His') . '.csv';
-}
+$fileName = $title . '-' . ($search ? 'Export' : 'Full_Export') . '-' . date('Ymd-His') . '.' . $extension;
 
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Content-Description: File Transfer');
-header("Content-type: text/csv");
-header("Content-Disposition: attachment; filename={$fileName}");
-header("Expires: 0");
-header("Pragma: public");
+header('Content-type: ' . $mediaType);
+header('Content-Disposition: attachment; filename=' . $fileName);
+header('Expires: 0');
+header('Pragma: public');
 
-$delimiter = get_option('csv_export_delimiter') ?: ',';
-if ($delimiter === 'tab') $delimiter = "\t";
-$enclosure = get_option('csv_export_enclosure') ?: '"';
-
-$file = fopen( 'php://output', 'w' );
+$file = fopen('php://output', 'w');
 
 fputcsv($file, $headers, $delimiter, $enclosure);
 
